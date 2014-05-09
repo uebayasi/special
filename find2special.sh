59c896af974a2d3b740b52f53f8e6b571ec4574a
Masao Uebayashi <uebayasi@tombi.co.jp>
2014-05-09 05:55:32 +0000

    Comments.

--- find2special.sh
+++ find2special.sh
@@ -23,6 +23,8 @@ fini()
 findall()
 {
 	/usr/bin/find ${paths} -print >${all}
+
+	# Rely on grep(1)'s -l/-L to identify binary files.
 	/usr/bin/xargs /usr/bin/grep -lI . <${all} >${txt}
 	/usr/bin/xargs /usr/bin/grep -LI . <${all} >${bin}
 }
@@ -40,19 +42,27 @@ dumpall()
 dumptxt()
 {
 	path2special ${txt} |
+
+	# Tag confidential files as nodiff to hide the contents.
 	/usr/bin/sed -e '/ mode=0600/s/$/ tags=nodiff/'
 }
 
 dumpbin()
 {
 	path2special ${bin} |
+
+	# Tag confidential files as nodiff to hide the contents.
 	/usr/bin/sed -e '/ mode=0600/s/$/ tags=nodiff/' |
+
+	# Exclude binary files from backup.
 	/usr/bin/sed -e '/ type=file/s/$/ tags=exclude/'
 }
 
 path2special()
 {
 	/usr/sbin/mtree -c -O ${1} |
+
+	# Convert to /etc/mtree/special* format
 	/usr/sbin/mtree -C -k type,mode,uname,gname |
 	/usr/bin/sed -e 's/ uname=root//; s/ gname=wheel//'
 }
